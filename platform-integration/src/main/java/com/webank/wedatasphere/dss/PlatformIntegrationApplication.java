3568f77609eb31154a8b09aeb56711e26687a4f4
sky <273606283@qq.com>
2025-02-26 11:28:37 +0800

    回滚

--- platform-integration/src/main/java/com/webank/wedatasphere/dss/PlatformIntegrationApplication.java
+++ platform-integration/src/main/java/com/webank/wedatasphere/dss/PlatformIntegrationApplication.java
@@ -3,7 +3,6 @@ package com.webank.wedatasphere.dss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 启动程序
@@ -11,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author wu
  */
 @SpringBootApplication
-@EnableDiscoveryClient  // 启用服务发现
 public class PlatformIntegrationApplication
 {
     public static void main(String[] args)
