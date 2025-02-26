b85b6f91d444115e5135d84e6b2c495f0da794c4
sky <273606283@qq.com>
2025-02-26 10:54:41 +0800

    添加平台整合服务注册功能

--- platform-integration/src/main/java/com/webank/wedatasphere/dss/PlatformIntegrationApplication.java
+++ platform-integration/src/main/java/com/webank/wedatasphere/dss/PlatformIntegrationApplication.java
@@ -3,6 +3,7 @@ package com.webank.wedatasphere.dss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 启动程序
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author wu
  */
 @SpringBootApplication
+@EnableDiscoveryClient  // 启用服务发现
 public class PlatformIntegrationApplication
 {
     public static void main(String[] args)
